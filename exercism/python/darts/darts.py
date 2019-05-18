9281823cb9049b1a906549e6262ca74c7d72b22b
Christopher Monaghan <48956409+cmonaghan76@users.noreply.github.com>
2019-05-18 12:46:59 -0400

    solution for darts.py

--- exercism/python/darts/darts.py
+++ exercism/python/darts/darts.py
@@ -1,2 +1,14 @@
+# Solution by Christopher Monaghan
+# May 18, 2019
+
 def score(x, y):
-    pass
+    import math
+    dart_distance = math.sqrt(x**2 + y**2)
+    if dart_distance >= 0 and dart_distance <= 1:
+        return 10
+    elif dart_distance > 1 and dart_distance <= 5:
+        return 5
+    elif dart_distance > 5 and dart_distance <= 10:
+        return 1
+    else:
+        return 0
