40b7840be0a8ff5212854e985dc24869e87c1314
Christopher Monaghan <48956409+cmonaghan76@users.noreply.github.com>
2019-05-18 13:35:18 -0400

    Resistor Color Duo solution

--- exercism/python/resistor-color-duo/resistor_color_duo.py
+++ exercism/python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,17 @@
 def value(colors):
-    pass
+    bandcolors = [
+        'black',
+        'brown',
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'violet',
+        'grey',
+        'white'
+    ]
+    values = list() #list to store numerical resistor values
+    for color in colors:
+        values.append(bandcolors.index(color))
+    return int(''.join(str(e) for e in values))
