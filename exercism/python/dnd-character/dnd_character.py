629b6cab0d3626493a729c75a58c9ecffac43e68
Christopher Monaghan <48956409+cmonaghan76@users.noreply.github.com>
2019-05-20 08:57:07 -0400

    solution to dnd exercise

--- exercism/python/dnd-character/dnd_character.py
+++ exercism/python/dnd-character/dnd_character.py
@@ -1,3 +1,22 @@
+import math
+import random
+
 class Character:
     def __init__(self):
-        pass
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def ability(self):
+        """ returns sum of highest 3-of-4 6-sided dice rolls"""
+        random.seed()
+        rolls = [random.randint(1,6) for i in range(4)]
+        return sum(sorted(rolls)[1:4])
+    
+def modifier(base):
+    """returns down-rounded DND modifier"""
+    return int(math.floor((base - 10) / 2))
