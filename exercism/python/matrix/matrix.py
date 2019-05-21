1d7cdf64f632976f870c8d3f30e7ea09d3c4fd98
Christopher Monaghan <48956409+cmonaghan76@users.noreply.github.com>
2019-05-21 15:28:18 -0400

    concise Matrix solution

--- exercism/python/matrix/matrix.py
+++ exercism/python/matrix/matrix.py
@@ -2,10 +2,7 @@
 
 class Matrix(object):
     def __init__(self, matrix_string):
-        self.matrix = list()
-        rows = matrix_string.split("\n")
-        for row in rows:
-            self.matrix.append([int(n) for n in row.split(" ")])
+        self.matrix = [[int(n) for n in row.split(" ")] for row in matrix_string.split("\n")]
 
     def row(self, index):
         return self.matrix[index-1]
