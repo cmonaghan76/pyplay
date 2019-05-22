eada2272a1cfa12455ab28d4fc07718cc59afe85
Christopher Monaghan <48956409+cmonaghan76@users.noreply.github.com>
2019-05-21 22:03:34 -0400

    Bank-Account solution

--- exercism/python/bank-account/bank_account.py
+++ exercism/python/bank-account/bank_account.py
@@ -1,18 +1,49 @@
+import threading
+lock = threading.Lock()
+
 class BankAccount(object):
     def __init__(self):
-        pass
+        self.status = 'closed'
 
     def get_balance(self):
-        pass
+        if self.status == 'open':
+            return self.balance
+        else:
+            raise ValueError('Balance unavailable. Account is closed')
 
     def open(self):
-        pass
+        if self.status == 'closed':
+            self.status = 'open'
+            self.balance = 0
+        else:
+            raise ValueError('Account already open.')
 
     def deposit(self, amount):
-        pass
+        if self.status == 'open':
+            if amount >= 0:
+                with lock:
+                    self.balance += amount
+            else:
+                raise ValueError('Cannot deposit a negative amount.')
+        else:
+            raise ValueError('Cannot deposit. Account is closed.')
 
     def withdraw(self, amount):
-        pass
+        if self.status == 'open':
+            if amount >= 0:
+                if self.balance - amount >= 0:
+                    with lock:
+                        self.balance -= amount
+                else:
+                    raise ValueError('Insufficient funds.')
+            else:
+                raise ValueError('Cannot withdraw a negative amount.')
+        else:
+            raise ValueError('Cannot withdraw. Account is closed.')
 
     def close(self):
-        pass
+        if self.status == 'open':
+            self.status = 'closed'
+        else:
+            raise ValueError('Account is already closed.')
+
