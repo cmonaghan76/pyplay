019d9eaf1c3110d1db861d3baae19253b3ecdbff
Christopher Monaghan <48956409+cmonaghan76@users.noreply.github.com>
2019-05-24 18:17:43 -0400

    revised Hamming solution

--- exercism/python/hamming/hamming.py
+++ exercism/python/hamming/hamming.py
@@ -1,9 +1,5 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands are not equal length.")
-    else:
-        count = 0
-        for a, b in zip(strand_a, strand_b):
-            if a != b:
-                count += 1
-        return count
+    distance = [a for a, b in zip(strand_a, strand_b) if a != b]
+    return len(distance)
