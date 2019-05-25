6744da00b60906d952dcd8c0d2461f5ae36f9500
Christopher Monaghan <48956409+cmonaghan76@users.noreply.github.com>
2019-05-25 07:55:25 -0400

    Raindrops solution

--- exercism/python/raindrops/raindrops.py
+++ exercism/python/raindrops/raindrops.py
@@ -1,2 +1,16 @@
+from functools import reduce
+
+
 def raindrops(number):
-    pass
+    sounds = {3: "Pling", 5: "Plang", 7: "Plong"}
+    factors = get_factors(number)
+    raindrops = ''.join([sounds[factor] for factor in sorted(factors) if factor in sounds.keys()])
+    if raindrops == '':
+        return str(number)
+    else:
+        return raindrops
+
+
+def get_factors(n):    
+    return set(reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
