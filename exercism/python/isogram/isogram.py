55575e31773be67bda73245d5edd68e2fc87e1ab
Christopher Monaghan <48956409+cmonaghan76@users.noreply.github.com>
2019-05-31 17:26:57 -0400

    isogram solution

--- exercism/python/isogram/isogram.py
+++ exercism/python/isogram/isogram.py
@@ -1,2 +1,7 @@
 def is_isogram(string):
-    pass
+    string = string.lower().translate({ord(i):None for i in ' -'})
+    duplicates = [letter for letter in string if string.count(letter) > 1]
+    if duplicates:
+        return False
+    else:
+        return True
