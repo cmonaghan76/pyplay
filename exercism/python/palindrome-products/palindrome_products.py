517a18e9e1a2ba2c31f084ed0d80421ee894660a
Christopher Monaghan <48956409+cmonaghan76@users.noreply.github.com>
2019-06-01 18:22:56 -0400

    palindrome-products solution

--- exercism/python/palindrome-products/palindrome_products.py
+++ exercism/python/palindrome-products/palindrome_products.py
@@ -1,6 +1,33 @@
-def largest_palindrome(max_factor, min_factor):
-    pass
+import itertools
 
 
-def smallest_palindrome(max_factor, min_factor):
-    pass
+def largest_palindrome(max_factor, min_factor=0):
+    palindromes = get_palindromes(max_factor, min_factor)
+    if len(palindromes) >= 1:
+        largest = max(palindromes.keys())
+        return (largest, set(palindromes[largest]))
+    else:
+        return (None,[])
+
+
+def smallest_palindrome(max_factor, min_factor=0):
+    palindromes = get_palindromes(max_factor, min_factor)
+    if len(palindromes) >= 1:
+        smallest = min(palindromes.keys())
+        return (smallest, set(palindromes[smallest]))
+    else:
+        return (None,[])
+
+
+def get_palindromes(max_factor, min_factor):
+    if min_factor > max_factor:
+        raise ValueError("min_factor must be smaller than max_factor")
+    palindromes = dict()
+    for a, b in itertools.combinations_with_replacement(range(min_factor, max_factor + 1), 2):
+        product = str(a * b)
+        if product == product[::-1]:
+            if int(product) in palindromes.keys():
+                palindromes[int(product)].append((a ,b))
+            else:
+                palindromes[int(product)] = [(a, b)]
+    return palindromes
